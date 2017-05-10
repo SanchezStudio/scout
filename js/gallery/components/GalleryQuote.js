627f695edfb027c5d0ddbce6d92740c89d7da171
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2017-05-10 10:58:47 -0400

    Update

--- js/gallery/components/GalleryQuote.js
+++ js/gallery/components/GalleryQuote.js
@@ -7,8 +7,9 @@ export default class GalleryQuote extends Component {
   render() {
     return (
       <div className="testimonial__quote" style={this.props.style}>
+        <div className="testimonial__double-quote quote">“</div>
         <div className="testimonial__content" dangerouslySetInnerHTML={{ __html: this.props.quote }}></div>
-        <div className="testimonial__author">{this.props.author}</div>
+        <h3 className="testimonial__author">{this.props.author}</h3>
       </div>
     )
   }
