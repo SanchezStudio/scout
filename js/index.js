02e6e3c9674f174cd7bbe9ec7114e7c102b3be22
Caleb Sanchez <calebmichaelsanchez@gmail.com>
2017-07-03 15:56:45 -0400

    Commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 require('smoothscroll-polyfill').polyfill();
 import AOS from 'aos';
-AOS.init();
 import './nav';
 import './home';
 import './gallery';
 import './listings';
+AOS.init();
+
+window.addEventListener("load", function() {
+  AOS.refresh();
+}, false);
--- js/listings/index.js
+++ js/listings/index.js
@@ -1,10 +1,30 @@
-import { procEvent } from "../util/helpers";
+import AOS from 'aos';
 
 !(function() {
-  let listings = document.getElementById("listings");
+  let listings = document.getElementById("listings-item");
 
   if (listings) {
-    procEvent(window.document, "DOMContentLoaded");
+    let images = document.querySelectorAll(".thumb-image");
+    let text = document.querySelectorAll(".html-block");
+    let map = document.querySelectorAll(".map-block");
+
+    function addAOS(element, animation, duration, offset) {
+      element.dataset.aos = animation;
+      element.dataset.aosDuration = duration;
+      element.dataset.offset = offset;
+    }
+
+    function cycle(elements) {
+      elements.forEach((item) => {
+        addAOS(item, "fade-up", "800", "150");
+      });
+    };
+
+    cycle(images);
+    cycle(text);
+    cycle(map);
+
+    AOS.refresh();
   }
 
-});
+})();
